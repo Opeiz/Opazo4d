35c5521f0abd4d5b8a34968d18a2f18edf06a9a7
Joaquin Opazo <ja.opazoy@gmail.com>
2023-04-24 16:21:14 +0200

    update

--- contrib/multi_domain_diag.py
+++ contrib/multi_domain_diag.py
@@ -93,7 +93,7 @@ def multi_domain_osse_metrics(tdat, test_domains, test_periods):
             print("== da_rec ==")
             print(da_rec)
             print("== da_ref ==")
-            print(da_ref[0])
+            print(da_ref[0,5])
 
             leaderboard_rmse = (
                 1.0 - (((da_rec - da_ref) ** 2).mean()) ** 0.5 / (((da_ref) ** 2).mean()) ** 0.5
