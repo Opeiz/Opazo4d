69490c602ba4eaec1571aafd8f59e844f29c1ec2
Joaquin Opazo <ja.opazoy@gmail.com>
2023-04-25 17:56:50 +0200

    update

--- test.py
+++ test.py
@@ -1,20 +1,42 @@
 import os
+import importlib
 
 def main():
-    def print_directory_tree(root_dir):
-        """
-        Recursively prints the directory tree starting at root_dir
-        """
-        for root, dirs, files in os.walk(root_dir):
-            level = root.replace(root_dir, '').count(os.sep)
-            indent = ' ' * 4 * (level)
-            print('{}{}/'.format(indent, os.path.basename(root)))
-            subindent = ' ' * 4 * (level + 1)
-            for f in files:
-                print('{}{}'.format(subindent, f))
+    def get_used_files(main_file):
+        used_files = set()
 
-    root_dir = '/homes/j22opazo/Documents/Stage/Opazo4d'
-    print_directory_tree(root_dir)
+        def explore_dependencies(file_path):
+            """
+            Recursively explore the dependencies of file_path and add them to used_files
+            """
+            if file_path in used_files:
+                return
+
+            used_files.add(file_path)
+
+            with open(file_path) as f:
+                content = f.read()
+
+            for line in content.split('\n'):
+                if line.startswith('from') or line.startswith('import'):
+                    parts = line.split()
+                    module_name = parts[1]
+                    try:
+                        module_path = importlib.import_module(module_name).__file__
+                        if os.path.isfile(module_path):
+                            explore_dependencies(module_path)
+                    except ModuleNotFoundError:
+                        pass
+
+        explore_dependencies(os.path.abspath(main_file))
+
+        return used_files
+    
+    main_file = '/homes/j22opazo/Documents/Stage/Opazo4d/main.py' 
+    used_files = get_used_files(main_file)
+
+    for file_path in used_files:
+        print(file_path)
 
 if __name__ == '__main__':
     main()
