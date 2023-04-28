cfe0785979fe8a713796609d7add64c94cf97bfd
Joaquin Opazo <ja.opazoy@gmail.com>
2023-04-28 09:51:35 +0200

    update

--- src/models.py
+++ src/models.py
@@ -230,6 +230,7 @@ class BilinAEPriorCost(nn.Module):
         )
 
     def forward_ae(self, x):
+        print("============ TEST ============")
         x = self.down(x)
         x = self.conv_in(x)
         x = self.conv_hidden(F.relu(x))
