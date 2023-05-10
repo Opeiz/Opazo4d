7dc051144c6d53ced618c0d59f8f29208ee47bfb
Joaquin Opazo <ja.opazoy@gmail.com>
2023-05-10 15:52:47 +0200

    update

--- contrib/multi_domain_diag.py
+++ contrib/multi_domain_diag.py
@@ -117,7 +117,8 @@ def multi_domain_osse_metrics(tdat, test_domains, test_periods):
     
     metrics_df = pd.concat(metrics).sort_values(by='mu')
     print("==== Metrics ====")
-    print(metrics_df.to_markdown()) 
+    print(metrics_df.to_markdown())
+    metrics_df.to_csv("multi_domain_metrics.csv")
 
     return metrics_df
 
